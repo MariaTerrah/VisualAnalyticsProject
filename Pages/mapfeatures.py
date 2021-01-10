2d3f4efd0d22d695faa09dafce5567454b85bce2
Minato-BAD <73838010+Minato-BAD@users.noreply.github.com>
2021-01-10 22:05:41 +0100

    Update mapfeatures.py

--- Pages/mapfeatures.py
+++ Pages/mapfeatures.py
@@ -74,7 +74,7 @@ def write():
     map = (
             (geoshape + chart)
                 .configure_view(strokeWidth=0)
-                .properties(height=400)
+                .properties(width=800, height=400)
                 .project("naturalEarth1")
         )
     st.altair_chart(map)
