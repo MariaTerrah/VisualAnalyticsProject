a6a15c9d9d8df310820296e683314ad02745e03d
Minato-BAD <73838010+Minato-BAD@users.noreply.github.com>
2021-01-10 22:11:20 +0100

    Update gdplife.py

--- Pages/gdplife.py
+++ Pages/gdplife.py
@@ -11,22 +11,21 @@ control_dataset = df
 # ------------------------------Bubble Plot: GDP/Life----------------------------------------------------------------------------
 def write():
     st.header("How GDP per Capita and Life Expectancy with population of countries are related.")
-    '''
+    st.markdown('''
         In the following bubble plot, we can identify GDP Capita on Y-axis and Life Expectancy (LifeExp) on X-axis.
-        The bubbles shows countries and shape of the bubbles shows their population values. So, bubbles are plotted on perticular values of X-axis and Y-axis.
-        
-        Usage:
-        
-            Please select year to see bubble plot visualization of three parameters GDPCapita (GDP capitalization), LifeExp (Life Expectancy ) and population for countries.
+        The bubbles shows countries and shape of the bubbles shows their population values. So, bubbles are plotted on perticular values of X-axis and Y-axis.''')
+    myexpander = st.beta_expander('Usage')
+    myexpander.write('''Please select year to see bubble plot visualization of three parameters GDPCapita (GDP capitalization), LifeExp (Life Expectancy ) and population for countries. 
             Size of the bubble shows population, bigger is the bubble, higher is the size.
-            Please hover on the bubbles to see values of GDPCapita, LifeExp and population with country names.
-            
-            Explanation:            
-                Life expectancy at birth is defined as the average number of years that a newborn could expect to live if he or she were to pass through life subject to the age-specific mortality rates of a given period.
+            Please hover on the bubbles to see values of GDPCapita, LifeExp and population with country names.''')
+    myExpander = st.beta_expander('Explanation')
+    myExpander.write('''Life expectancy at birth is defined as the average number of years that a newborn could expect to live if he or she were to pass through life subject to the age-specific mortality rates of a given period.
                 GDP per capita is gross domestic product divided by midyear population. GDP is the sum of gross value added by all resident producers in the economy plus any product taxes and minus any subsidies not included in the value of the products.
                 Bubble plot shows the improvement in GDP Capita increases life expectancy. And plot is shifting to the right as we shift in the year.
     
-    '''
+    ''')
+        
+           
     select_year = alt.selection_single(
         name='select', fields=['Year'], init={'Year': 2000},
         bind=alt.binding_range(min=2000, max=2018, step=1)
