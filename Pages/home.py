3550ec93ce2cce966ace0614be46205cc1e0cba3
Minato-BAD <73838010+Minato-BAD@users.noreply.github.com>
2021-01-10 21:29:23 +0100

    Update home.py

--- Pages/home.py
+++ Pages/home.py
@@ -14,14 +14,11 @@ control_dataset = df
 def write():
     st.title("Population Analytics")
     
-    st.header("How are population, density, poverty and other parameters spread across the world ? ")
-    
-    '''
-    When it comes to learning more about the population distribution over the world, one can certainly wonder first where are the most populated areas. However, this isn’t enough to know who lives there and how. 
-    Therefore, we decided to add some useful features, when available, like density, poverty, literacy rate, GDP per capita, GINI index, rural population, population living in a city of more than 1 million inhabitants, unemployment rate, life expectancy, fertility rate and distribution of the population in 3 segments (children, labor force, elder people). 
-    This enable us to know more about the status of each country : for example China may be a highy populated country as India but the difference is that India is a younger country.
-    All the data are coming from the World Bank OpenData website, you can find the original dataset and the metadata on the github.
-    '''
+    st.header('How are population, density, poverty and other parameters spread across the world ?')
+
+    st.markdown('When it comes to learning more about the population distribution over the world, one can certainly wonder first where are the most populated areas. However, this isn’t enough to know who lives there and how. Therefore, we decided to add some useful features, when available, like density, poverty, literacy rate, GDP per capita, GINI index, rural population, population living in a city of more than 1 million inhabitants, unemployment rate, life expectancy, fertility rate and distribution of the population in 3 segments (children, labor force, elder people). This enable us to know more about the status of each country : for example China may be a highy populated country as India but the difference is that India is a younger country.')
+    st.markdown('All the data are coming from the _World Bank OpenData_ website, you can find the original dataset and the metadata on the github.')
+
     #Disclaimers----------------------------------------------------------------------------
     st.header('Disclaimers')
     ''' 
