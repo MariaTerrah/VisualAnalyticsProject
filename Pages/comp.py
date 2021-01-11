ca3c4f9a5cfe968a9e887e255371bec2bf8aba8f
Minato-BAD <73838010+Minato-BAD@users.noreply.github.com>
2021-01-11 01:19:25 +0100

    Update comp.py

--- Pages/comp.py
+++ Pages/comp.py
@@ -42,7 +42,7 @@ def write():
         as_=['Population Status', 'Proportion (%)']
     ).mark_area(
     opacity=0.5,
-    order=True
+    line=True
     ).encode(
         alt.X('Year:O'),
         alt.Y('Proportion (%):Q', stack=None),
